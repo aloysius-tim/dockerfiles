865f3d9075c2b72a0a80947b1e958c632b7a68ca
SV Lentink <sv@lent.ink>
2016-07-18 15:59:58 +0200

    Removed the RUN prefix (copy error from Dockerfile)

--- laravel-nginx/run_after_copying_repo.sh
+++ laravel-nginx/run_after_copying_repo.sh
@@ -5,8 +5,8 @@ set -e
 # and preform some basic required actions.
 
 # if dir exist, do not produce an error (|| true)
-RUN mkdir /repo/bootstrap/cache || true
-RUN /usr/local/bin/composer update --no-scripts
-RUN mkdir /var/www/storage/framework/views || true
-RUN chown www-data:www-data -R /var/www
-RUN chmod 777 -R /var/www/storage
+mkdir /repo/bootstrap/cache || true
+/usr/local/bin/composer update --no-scripts
+mkdir /var/www/storage/framework/views || true
+chown www-data:www-data -R /var/www
+chmod 777 -R /var/www/storage
