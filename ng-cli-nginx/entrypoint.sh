ed13d7977bd5e8f1c67daf33b22645e73691888b
Jurien Hamaker <jurienhamaker@gmail.com>
2016-07-31 18:35:10 +0200

    npm install based on production mode or not

--- ng-cli-nginx/entrypoint.sh
+++ ng-cli-nginx/entrypoint.sh
@@ -3,8 +3,6 @@
 # cd to the project
 cd /project || exit
 
-# make sure everything is installed for the project.
-npm install
 
 # start nginx
 service nginx start
@@ -14,7 +12,11 @@ echo "$APP_DEBUG"
 
 # if in debug mode, start the serve command for auto reload.
 if [ "$APP_DEBUG" == true ] ; then
+	# make sure everything is installed for the project.
+	npm install
 	ng serve
 else
+	# make sure everything is installed for the project.
+	npm install --only=production
 	tail -f /dev/null
 fi
