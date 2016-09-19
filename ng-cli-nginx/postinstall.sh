17dc92a8b33fcefcf27710102db9e49f63bf9cb7
Jurien <jurien@kings-dev.io>
2016-09-19 15:06:55 +0200

    updated angular-cli version

--- ng-cli-nginx/postinstall.sh
+++ ng-cli-nginx/postinstall.sh
@@ -1,2 +1,2 @@
 apt-get update && apt-get install -y nginx
-npm cache clean && npm install -gf angular-cli@webpack @angular/tsc-wrapped typings
+npm cache clean && npm install -gf angular-cli@1.0.0-beta.14 @angular/tsc-wrapped typings
