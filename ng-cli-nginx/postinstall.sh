ef50195c9b97f6f3e444bbb951492dee81a35d96
Unknown <jurienhamaker@gmail.com>
2016-10-31 16:50:56 +0100

    apparently ng-cli broke when upgrading npm

--- ng-cli-nginx/postinstall.sh
+++ ng-cli-nginx/postinstall.sh
@@ -5,5 +5,4 @@ echo "events { worker_connections 1024; }" >> /etc/nginx/nginx.conf
 rm -rf /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/angular /etc/nginx/sites-enabled/default
 
-npm install -gf npm
 npm install -gf angular-cli @angular/tsc-wrapped typings typescript
