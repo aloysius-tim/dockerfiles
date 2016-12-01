5e28ef756522e793364db90d4922f33cec8f3904
Unknown <jurienhamaker@gmail.com>
2016-12-01 17:06:06 +0100

    added touches for files

--- ng-cli-nginx/postinstall.sh
+++ ng-cli-nginx/postinstall.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 apt-get update && apt-get install -y nginx
 echo "daemon off;" >> /etc/nginx/nginx.conf
-echo "events { worker_connections 1024; }" >> /etc/nginx/nginx.conf
+# echo "events { worker_connections 1024; }" >> /etc/nginx/nginx.conf
+touch /var/log/nginx/error.log
+touch /var/log/nginx/access.log
+
 rm -rf /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/angular /etc/nginx/sites-enabled/default
 
