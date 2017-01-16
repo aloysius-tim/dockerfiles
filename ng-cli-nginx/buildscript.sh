9b66805df830fa6bfd70cc188e83fc589931768a
Jurien Hamaker <jurien@superbuddy.nl>
2017-01-16 17:47:56 +0100

    build script into command & output

--- ng-cli-nginx/buildscript.sh
+++ ng-cli-nginx/buildscript.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+cd /project
 
-npm install
+npm install | tee /build.log
 
 # make sure everything is installed for the project.
 if [ "$AOT" == true ] ; then
 	echo "AOT enabled, using AOT"
-	ng build -prod --aot
+	ng build -prod --aot | tee /build.log
 else
-	ng build -prod
+	ng build -prod | tee /build.log
 fi
 
 rm -rf /build
